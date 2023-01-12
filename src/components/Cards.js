c39d5507074c7f0a0fdce366b1f9ff6da74d6696
SAMY COLLIARD <samy3876@gmail.com>
2023-01-12 17:23:36 +0100

    MaJ Modal

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SuperVilain from "../assets/images/SuperVilain.png"
-
+import Joker from "../assets/images/370-joker.jpg"
+import Approved from "../assets/images/approved.png"
 
 function Cards({closeModal}) {
     return (
@@ -10,40 +11,49 @@ function Cards({closeModal}) {
                 <button onClick={()=>closeModal(false)}>X</button>
                 <h1 className="wanted">WANTED</h1>
                 <ul className="powerList">
-                    <li>pouvoir 1</li>
-                    <li>pouvoir 2</li>
-                    <li>pouvoir 3</li>
+                    <li>Genie Criminel</li>
+                    <li>Chimiste</li>
+                    <li>Folies</li>
                 </ul>
             </header>
             <body>
                 <div className="profilPicture">
-                    <img src={SuperVilain} alt="mechant"/>
+                    <img src={Joker} alt="mechant"/>
                 </div>
                 <div>
-                    <ul>
-                        <li>pseudo</li>
-                        <li>cruauté</li>
-                        <li>force</li>
-                        <li>intelligence</li>
-                        <li>espece</li>
-                        <li>taille</li>
-                        <li>popularité</li>
+                    <ul className="caractPerso">
+                        <li className="l1"><h3>PRIX</h3>
+                        <p>1000K</p></li>
+                        <li className="l2"><h3>CRUAUTE</h3>
+                        <p>100/100</p></li>
+                        <li className="l3"><h3>FORCE</h3>
+                        <p>10/100</p></li>
+                        <li className="l4"><h3>INTELLIGENCE</h3>
+                        <p>100/100</p></li>
+                        <li className="l5"><h3>ESPECE</h3>
+                        <p>Humain</p></li>
+                        <li className="l6"><h3>TAILLE</h3>
+                        <p>196cm</p></li>
+                        <li className="l7"><h3>POPULARITE</h3>
+                        <p>100/100</p></li>
+                        <li className="l8"><img src={Approved} alt="tampon"></img></li>
                     </ul>
-                    <div>
-                        <p>descritption</p>
-                    </div>
                 </div>  
+                
             </body>
             <footer>
-                <div>
+            <div className="description">
+                        <p >Le Joker affiche un goût particulier pour les farces macabres. Les armes chimiques n'ont pas de secret pour lui et il porte en permanence plusieurs sortes d’acides ainsi que son venin de Joker. Sans oublier le pistolet d'où sort un drapeau 'BANG!'</p>
+                    </div>
+                <div className="highFooter">
                     <h3>DO NOT APPROACH</h3>
                     <h3>EXTREMELY DANGEROUS</h3>
                 </div>
-                <h2>NAME OF VILAINT</h2>
+                <h2>J    O    K    E    R</h2>
             </footer>
         </div>
         <div>
-            <button>ADD TO SHOPPING CART</button>
+            <button className="buttonShop" onClick={()=>closeModal(false)}>ADD TO SHOPPING CART</button>
         </div>
       </div>
     )
