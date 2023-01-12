fa51b204250147148bb0cd2cf9885dc8c43c3591
sarahcharbit <sarahcharbit000@gmail.com>
2023-01-12 16:31:47 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './screens/Home'
-
+import Panier from './components/Panier'
+import ModalPaiement from './components/ModalPaiement'
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/panier' element={<Panier />} />
+        <Route path='/modalPaiement' element={<ModalPaiement />} />
       </Routes>
     </div>
   )
