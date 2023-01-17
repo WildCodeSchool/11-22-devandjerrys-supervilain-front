a142ef8454d8003858c6416faef25f4e2f89bd97
Helene <helene.blanche@hotmail.fr>
2023-01-17 16:35:16 +0100

    Page Message created and add changes on inscription component

--- src/components/Inscription.js
+++ src/components/Inscription.js
@@ -1,142 +1,91 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Blood from '../assets/images/blood.png'
+import Message from './Message'
 
-const user = {
-  pseudo: { pseudo: 'Joker' },
-  picture: {
-    md: 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/370-joker.jpg'
-  },
-  passWord: 'IloveBatou',
-  passWordConf: { passWord: 'IloveBatou' }
-}
-// constructor (props) {
-//   super(props)
-//   this.state = {
-//     pseudo: '',
-//     passWord:'',
-//     passWord2 :''
-//   }
-//     this.handleChange = this.handleChange.bind(this)
-// }
 const Inscription = () => {
   const [pseudo, setPseudo] = useState('')
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
   const [passWord, setPassWord] = useState('')
   const [passWordConf, setPassWordConf] = useState('')
+  const [vilain, setVilain] = useState('')
+  const [messageConf, setMessageConf] = useState('')
+  const [imageBlood, setImageBlood] = useState(false)
+
   const handleSubmit = e => {
     e.preventDefault()
+    if (vilain.includes(pseudo) || passWord !== passWordConf) {
+      setMessageConf(
+        `NOPE !!! le pseudo est déjà présent ou le mot de passe n'est pas conforme `
+      )
+    } else {
+      setMessageConf(`WELCOME  : ${pseudo}`)
+    }
+    setIsUserLoggedIn(prevState => !prevState)
   }
   useEffect(() => {
-    // console.log(pseudo)
-  }, [pseudo])
-
-  const [userVerif, setUserVerif] = useState(user)
-
-  const verifUser = () => {
-    // Send the request
     axios
-      .get(
-        'https://github.com/WildCodeSchool/11-22-devandjerrys-supervilain-p2-back.git'
-      )
-      // Extract the DATA from the received response
+      .get('http://localhost:4242/catalogue/vilain')
       .then(response => response.data)
-      // Use this data to update the state
       .then(data => {
-        // console.log(data)
-
-        setUserVerif(data.results[0])
+        setVilain(data.map(element => element.pseudo))
       })
-
-    return (
-      <div className='App'>
-        <button type='button' onClick={verifUser}>
-          User
-        </button>
-
-        <Inscription
-          pseudo={userVerif.pseudo}
-          images={userVerif.images.md}
-          passWord={userVerif.passWord}
-        />
-      </div>
-    )
+  }, [])
+  function blood() {
+    setImageBlood(!imageBlood)
   }
 
   return (
-    <div className='Inscription'>
-      <h3>INSCRIVEZ-VOUS !</h3>
-      <div className='Inscription-pseudo'>
-        <form onSubmit={handleSubmit}>
-          <div className='Inscription-pseudo'>
-            pseudo :
-            <input
-              type='text'
-              required
-              placeholder='Enter'
-              value={pseudo}
-              onChange={e => setPseudo(e.target.value)}
-            />
-          </div>
-          <div className='Inscription-MDP'>
-            mot de passe :
-            <input
-              type='text'
-              size='8'
-              required
-              placeholder=''
-              value={passWord}
-              onChange={e => setPassWord(e.target.value)}
-            />
-          </div>
-          <div className='Inscription-MDP2'>
-            {' '}
-            confirmer le mot de passe :
-            <input
-              type='text'
-              required
-              size='8'
-              placeholder=''
-              value={passWordConf}
-              onChange={e => setPassWordConf(e.target.value)}
-            />
-          </div>
-          <button
-            type='submit'
-            value='submit'
-            onClick={
-              () => setIsUserLoggedIn(prevState => !prevState)
-              // .then(value =>  'reset')
-            }
-          >
-            GO
-          </button>
-
-          {/* <img className='blood' src='../src/asset/images/Blood-Splatter.png'>
-            blood
-          </img> */}
-        </form>
-        {isUserLoggedIn && <Message />}
+    <div className='All'>
+      <div className='Inscription'>
+        <h3>INSCRIVEZ-VOUS !</h3>
+        <div className='Inscription-pseudo'>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <div className='Inscription-pseudo'>
+                pseudo :
+                <input
+                  type='text'
+                  required
+                  placeholder='Enter'
+                  value={pseudo}
+                  onChange={e => setPseudo(e.target.value)}
+                />
+              </div>
+              <div className='Inscription-MDP'>
+                mot de passe :
+                <input
+                  type='text'
+                  size='8'
+                  required
+                  placeholder=''
+                  value={passWord}
+                  onChange={e => setPassWord(e.target.value)}
+                />
+              </div>
+              <div className='Inscription-MDP2'>
+                {' '}
+                confirmer le mot de passe :
+                <input
+                  type='text'
+                  required
+                  size='8'
+                  placeholder=''
+                  value={passWordConf}
+                  onChange={e => setPassWordConf(e.target.value)}
+                />
+              </div>
+            </label>
+            <button onMouseEnter={blood} type='submit' value='submit'>
+              GO
+              {imageBlood && <img className='bloodSplatter' src={Blood} />}
+            </button>
+          </form>
+          {isUserLoggedIn && <Message messageConf={messageConf} />}
+        </div>
       </div>
     </div>
   )
 }
-function Message() {
-  useEffect(() => {}, [])
-  return <h1 className='welcome'>HI, YOU'RE WELCOME</h1>
-}
-// function Message2(props) {
-//   const { isUserLoggedIn } = props
-//   if (isUserLoggedIn) {
-//     return <h1 className='welcome'>HI, YOU'RE WELCOME</h1>
-//   }
-
-//   return <h1 className='no'>NOPE!!!</h1>
-// }
-
-// {studentList.filter((student) => !showFailedOnly || student.note < 10).map((student, index) => (
-//   <li key={index}>
-//     Name: {student.name} Note: {student.note}
-//   </li>
-// ))}
 
 export default Inscription
---
+++ src/components/Message.js
@@ -0,0 +1,8 @@
+function Message(props) {
+  return (
+    <div className='message'>
+      <p>{props.messageConf}</p>
+    </div>
+  )
+}
+export default Message
